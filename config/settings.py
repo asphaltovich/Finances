1bdb523aa5e93ee6595ca991f168c82e589328e1
asphaltovich <asphaltartem@yandex.ru>
2026-06-04 18:49:28 +0700

    Подключение БД

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '7860120107',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
